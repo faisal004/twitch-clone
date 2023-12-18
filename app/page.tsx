8cb142fa082809372d4bb6a771cb4b7bd6907dee
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-12-18 21:58:40 +0530

    added : authentication added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
+
 
 export default function Home() {
   return (
-    <div className='text-6xl text-amber-200'>Hello World!!!!</div>
+    <div className='text-6xl text-amber-200'>
+      <UserButton
+      afterSignOutUrl='/'
+      />
+    </div>
   )
 }
