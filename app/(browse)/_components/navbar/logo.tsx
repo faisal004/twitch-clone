184daf1d75ff141980fd929e92065bb72cb7d285
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-12-23 22:33:19 +0530

    added : Navbar added

---
+++ app/(browse)/_components/navbar/logo.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import { Poppins } from 'next/font/google'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
+})
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="flex lg:flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
+          <Image src="/squarylines.svg" alt="LiveHub" width="32" height="32" />
+        </div>
+        <div className={cn('hidden lg:block', font.className)}>
+          <p className="text-lg font-semibold">LiveHub</p>
+          <p className="text-xs text-muted-foreground">Let&apos;s play</p>
+        </div>
+      </div>
+    </Link>
+  )
+}
