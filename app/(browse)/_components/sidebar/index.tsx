531cb3b602392c0925e282233df5b6bcc01715e8
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-12-25 13:38:22 +0530

    added : recommended user will be displayed also live badge added

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,11 +1,25 @@
-import Toggle from './toggle'
+import getRecommended from '@/lib/get-recommended'
+import Recommended, { RecommendedSkeleton } from './recommended'
+import Toggle, { ToggleSkeleton } from './toggle'
 import Wrapper from './wrapper'
 
-const Sidebar = () => {
+const Sidebar = async () => {
+  const recommended = await getRecommended()
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommended data={recommended} />
+      </div>
     </Wrapper>
+  )
+}
+export const SidebarSkeleton = () => {
+  return (
+    <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      <ToggleSkeleton />
+      <RecommendedSkeleton />
+    </aside>
   )
 }
 
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,9 +1,21 @@
 'use client'
 import { cn } from '@/lib/utils'
 import { useSidebar } from '@/store/use-sidebar'
+import { useIsClient } from 'usehooks-ts'
+import { ToggleSkeleton } from './toggle'
+import { RecommendedSkeleton } from './recommended'
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  const isClient = useIsClient()
   const { collapsed } = useSidebar((state) => state)
+  if (!isClient) {
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    )
+  }
   return (
     <aside
       className={cn(
