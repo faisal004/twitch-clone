56d7d17f6df8eb04d6f93832615c47b302c846e8
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-12-25 17:54:09 +0530

    fixed : user other than loggedin user will be recommended

--- lib/get-recommended.ts
+++ lib/get-recommended.ts
@@ -1,12 +1,33 @@
 
+import { getSelf } from './auth-service';
 import { db } from './db'
 
 const getRecommended = async () => {
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-  })
+  let userId;
+  try {
+    const self = await getSelf()
+    userId = self.id
+
+  } catch (error) {
+    userId = null
+  }
+  let users = []
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          id: userId
+        }
+      }
+    })
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+  }
+
   return users
 }
 
