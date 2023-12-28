54256a07b736255ea62e51c16a5c9493f450c362
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-12-28 11:48:14 +0530

    added : block and unblock actions added

---
+++ actions/block.ts
@@ -0,0 +1,20 @@
+"use server"
+import { blockUser, unblockUser } from "@/lib/block-service"
+import { revalidatePath } from "next/cache"
+
+export const onBlock = async (id: string) => {
+    const blockedUser = await blockUser(id)
+    revalidatePath("/")
+    if (blockedUser) {
+        revalidatePath(`/${blockedUser.blocked.username}`)
+    }
+    return blockedUser
+}
+export const unBlock = async (id: string) => {
+    const unblockedUser = await unblockUser(id)
+    revalidatePath("/")
+    if (unblockedUser) {
+        revalidatePath(`/${unblockedUser.blocked.username}`)
+    }
+    return unblockedUser
+}
