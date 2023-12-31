6377192f791afea29e8e9520c82925a4669e39d3
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-12-31 16:01:24 +0530

    added : new service added

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -4,9 +4,22 @@ const getUserByUsername = async (username: string) => {
     const user = await db.user.findUnique({
         where: {
             username
+        },
+        include: {
+            stream: true
         }
     })
     return user
 }
 
 export default getUserByUsername;
+
+export const getUserById = async (id: string) => {
+    const user = await db.user.findUnique({
+        where: { id },
+        include: {
+            stream: true,
+        },
+    });
+    return user;
+}
