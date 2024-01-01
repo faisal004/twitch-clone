2e84d6566bd17a7014719b02b2b6b98efbc2608f
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-01 11:21:13 +0530

    added : stream completed with chat and live stream video

--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -9,6 +9,7 @@ import { ConnectionState, Track } from 'livekit-client'
 import { OfflineVideo } from './offline-video'
 import { LoadingVideo } from './loading-video'
 import { LiveVideo } from './live-video'
+import { Skeleton } from '../ui/skeleton'
 
 interface VideoProps {
   hostName: string
@@ -24,11 +25,18 @@ export const Video = ({ hostIdentity, hostName }: VideoProps) => {
   ]).filter((track) => track.participant.identity === hostIdentity)
   let content
   if (!participant && connectionState === ConnectionState.Connected) {
-    content = <OfflineVideo username={hostName}/>
+    content = <OfflineVideo username={hostName} />
   } else if (!participant || tracks.length === 0) {
-    content = <LoadingVideo label={connectionState}/>
+    content = <LoadingVideo label={connectionState} />
   } else {
-    content = <LiveVideo participant={participant}/>
+    content = <LiveVideo participant={participant} />
   }
-  return <div className='aspect-video border-b group relative:'>{content}</div>
+  return <div className="aspect-video border-b group relative:">{content}</div>
+}
+export const VideoSkeleton = () => {
+  return (
+    <div className="aspect-video border-x border-background">
+      <Skeleton className="h-full w-full rounded-none" />
+    </div>
+  )
 }
