bb6ee73a5c22fcaafe3c007a7c8077ead6293ce4
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-01 21:54:27 +0530

    fix : now user service will return followers count also

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -6,7 +6,12 @@ const getUserByUsername = async (username: string) => {
             username
         },
         include: {
-            stream: true
+            stream: true,
+            _count:{
+                select:{
+                    followedBy:true
+                }
+            }
         }
     })
     return user
