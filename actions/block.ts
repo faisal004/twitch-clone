f607c81bd2c64fc17c84f5fe7be47843ca0170bc
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-01 22:27:31 +0530

    fix : now you can kick guest users

--- actions/block.ts
+++ actions/block.ts
@@ -1,20 +1,41 @@
-"use server"
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
+
+import { getSelf } from "@/lib/auth-service";
 import { blockUser, unblockUser } from "@/lib/block-service"
-import { revalidatePath } from "next/cache"
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!,
+);
 
 export const onBlock = async (id: string) => {
-    const blockedUser = await blockUser(id)
-    revalidatePath("/")
-    if (blockedUser) {
-        revalidatePath(`/${blockedUser.blocked.username}`)
+    const self = await getSelf();
+
+    let blockedUser;
+
+    try {
+        blockedUser = await blockUser(id);
+    } catch {
+        // This means user is a guest
     }
-    return blockedUser
-}
-export const unBlock = async (id: string) => {
-    const unblockedUser = await unblockUser(id)
-    revalidatePath("/")
-    if (unblockedUser) {
-        revalidatePath(`/${unblockedUser.blocked.username}`)
+    try {
+        await roomService.removeParticipant(self.id, id);
+    } catch {
+        // This means user is not in the room
     }
-    return unblockedUser
-}
+    revalidatePath(`/u/${self.username}/community`);
+
+    return blockedUser;
+};
+
+export const onUnblock = async (id: string) => {
+    const self = await getSelf();
+    const unblockedUser = await unblockUser(id);
+
+    revalidatePath(`/u/${self.username}/community`);
+    return unblockedUser;
+};
