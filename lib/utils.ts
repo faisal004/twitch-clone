33689f9e4f0a315379746aba9ccf531873702d45
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-01 11:15:14 +0530

    added : utility function added to add color to username

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import stc from "string-to-color";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const stringToColor = (str: string) => {
+  return stc(str);
+};
