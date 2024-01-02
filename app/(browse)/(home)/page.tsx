a1c9255ed7e85167eac20f915b8ba33c87d20e67
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-02 10:59:01 +0530

    added : home page added ,where all streams can be seen

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,10 +1,14 @@
-import { UserButton } from '@clerk/nextjs'
+import { Suspense } from "react";
+import { ResultSkeleton, Results } from "./_components/results";
 
 
 export default function Home() {
   return (
-    <div className='text-6xl text-amber-200'>
-      JEllo
+    <div className='h-full p-8 max-w-screen-2xl mx-auto'>
+      <Suspense fallback={<ResultSkeleton/>}>
+      <Results/>
+
+      </Suspense>
     </div>
   )
 }
