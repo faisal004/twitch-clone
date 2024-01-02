63dad0a9727b2da119b59ec5905739ba5ebbf11c
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-02 11:52:10 +0530

    fix : handling edgecase if user get deleted while streaming

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,6 +2,7 @@ import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { db } from '@/lib/db'
+import { resetIngresses } from '@/actions/ingress'
 
 export async function POST(req: Request) {
 
@@ -87,6 +88,7 @@ export async function POST(req: Request) {
         })
     }
     if (eventType == "user.deleted") {
+        await resetIngresses(payload.data.id);
         await db.user.delete({
             where: {
                 externalUserId: payload.data.id,
